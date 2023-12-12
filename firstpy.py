94fd5e406022a793feb40fc7d454aacf8bff9d4c
Buntariga2545 <buntariga.ch@kkumail.com>
2023-12-12 11:47:08 +0700

    test run .py

--- firstpy.py
+++ firstpy.py
@@ -1,1 +1,34 @@
-print("test")
+import argparse
+
+def parse_input():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--num',
+        type=int,
+        required=True,
+        help='input for the multiplyby9 function'
+    )
+
+    parser.add_argument(
+        '--XX',
+        type=int,
+        default=7,
+        help='input for XX'
+    )
+    
+    args = parser.parse_args()
+    return args
+
+def printHello():
+    print("Hello World!")
+
+def multiplyby9(inputV):
+    print(9*inputV)
+
+if __name__ == "__main__":
+    input_v = parse_input()
+    print(f'the input num is {input_v.XX}')
+    print('we are in the main function')
+    multiplyby9(20)
+    printHello()
