ac2f6eea62439315e8c300fafd9929b6cf4317b3
Buntariga2545 <buntariga.ch@kkumail.com>
2023-12-25 15:54:02 +0700

    add delete folder

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,6 +1,6 @@
 import subprocess #สำหรับรัน terminal command
 
-if __name__ == "__main__:
+if __name__ == "__main__":
     # basic terminal command
     subprocess.run(["ls","-ltr"])
     subprocess.run(["rm","-r","/home/buntariga/testfolder1"])
