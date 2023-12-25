5d3f865cd7ed3304bef1bca790d1a78c61427ceb
Buntariga2545 <buntariga.ch@kkumail.com>
2023-12-25 16:23:57 +0700

    test subprocess

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,6 +1,23 @@
-import subprocess #สำหรับรัน terminal command
+from multiprocessing import process
+import subprocess
+from sys import stdout #สำหรับรัน terminal command
 
 if __name__ == "__main__":
     # basic terminal command
-    subprocess.run(["ls","-ltr"])
-    subprocess.run(["rm","-r","/home/buntariga/testfolder1"])
+    print('first run num=100 XX=90')
+    subprocess.run(["python","firstpy.py", "--num", "100", "--XX", "90"])
+    print('------------------------------------------------------')
+    print('second run num=-10 XX=-90')
+    subprocess.run(["python","firstpy.py", "--num", "-10", "--XX", "-90"])
+    print('------------------------------------------------------')
+    print('third run num=0')
+    subprocess.run(["python","firstpy.py", "--num", "0"])
+    print('------------------------------------------------------')
+
+    process_output = subprocess.Popen(["python", "firstpy.py", "--num", "0"],
+                                      stdout=subprocess.PIPE,
+                                      stdout=subprocess.PIPE)
+    out, err = process_output.communicate()
+    print(out.decode('utf-8'))
+    print(len(out.decode(utf-8)))
+                                      
