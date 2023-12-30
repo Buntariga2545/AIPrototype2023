5e442775e4cf72d1fb945eff76b14bfcbdcb6712
Buntariga2545 <buntariga.ch@kkumail.com>
2023-12-30 17:38:55 +0700

    test subprocess

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,23 +1,29 @@
-from multiprocessing import process
-import subprocess
-from sys import stdout #สำหรับรัน terminal command
+from re import sub
+import subprocess #สำหรับรัน terminal command
 
 if __name__ == "__main__":
     # basic terminal command
-    print('first run num=100 XX=90')
-    subprocess.run(["python","firstpy.py", "--num", "100", "--XX", "90"])
-    print('------------------------------------------------------')
-    print('second run num=-10 XX=-90')
-    subprocess.run(["python","firstpy.py", "--num", "-10", "--XX", "-90"])
-    print('------------------------------------------------------')
-    print('third run num=0')
-    subprocess.run(["python","firstpy.py", "--num", "0"])
-    print('------------------------------------------------------')
+    #subprocess.run(["ls", "-ltr"])
+    #subprocess.run(["rm", "-r", "/home/dewbtrg/testfolder1"])
 
+    #python commands
+    print(f"first run num=100 XX=90")
+    subprocess.run(["python", "firstpy.py", "--num", "100", "--XX", "90"])
+    print(f"------------------------------------------------------")
+    print(f"second run num=-10 XX=-90")
+    subprocess.run(["python", "firstpy.py", "--num", "-10", "--XX", "-90"])
+    print(f"------------------------------------------------------")
+    print(f"third run num=0")
+    subprocess.run(["python", "firstpy.py", "--num", "0"])
+    print(f"------------------------------------------------------")
+
+
+          
+    #use output from other program
     process_output = subprocess.Popen(["python", "firstpy.py", "--num", "0"],
                                       stdout=subprocess.PIPE,
-                                      stdout=subprocess.PIPE)
+                                      stderr=subprocess.PIPE)
     out, err = process_output.communicate()
     print(out.decode('utf-8'))
-    print(len(out.decode(utf-8)))
+    print(len(out.decode('utf-8')))
                                       
