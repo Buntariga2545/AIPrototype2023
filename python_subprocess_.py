dfc75e3402731ef03b79f0348b6690f00c361678
Buntariga2545 <buntariga.ch@kkumail.com>
2024-01-02 11:29:50 +0700

    HW

--- python_subprocess_.py
+++ python_subprocess_.py
@@ -34,14 +34,14 @@ if __name__ == "__main__":
     p3 = subprocess.run(["python", "firstpy.py", "--num", "0"], capture_output=True)
     print(f"------------------------------------------------------\n")
 
-    result1 = extract_numeric(p1.stdout.decode('utf-8'))
-    result2 = extract_numeric(p2.stdout.decode('utf-8'))
-    result3 = extract_numeric(p3.stdout.decode('utf-8'))
+    output1 = extract_numeric(p1.stdout.decode('utf-8'))
+    output2 = extract_numeric(p2.stdout.decode('utf-8'))
+    output3 = extract_numeric(p3.stdout.decode('utf-8'))
 
 #    print(f"Result 1: {result1}")
 #    print(f"Result 2: {result2}")
 #    print(f"Result 3: {result3}")
 
-    total_sum = sum([result1, result2, result3])
-    print(f"Summation of {result1} + ({result2}) + {result3} is: {total_sum}")
+    total_sum = sum([output1, output2, output3])
+    print(f"Summation of: {output1} + ({output2}) + {output3} = {total_sum}")
     printHello()
