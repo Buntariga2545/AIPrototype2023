d8a091ebfa3b6a9dd7fce56c023a7d050cd5998c
Buntariga2545 <buntariga.ch@kkumail.com>
2024-01-16 11:12:51 +0700

    link with html

--- firstflask.py
+++ firstflask.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, make_response
+from flask import Flask, redirect, request, render_template, make_response
 
 import json
 import sys
@@ -32,6 +32,35 @@ def homefn():
         print(namein, file=sys.stdout)
         print(lastnamein, file=sys.stdout)
         return render_template("home.html",name=namein)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save('file')
+        return render_template("home.html",name='upload completed')
+        # check if the post request has the file part
+        # if 'file' not in request.files:
+        #     flash('No file part')
+        #    return redirect(request.url)
+    
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        # if file.filename == '':
+        #     flash('No selected file')
+        #     return redirect(request.url)
+
+        #return redirect(url_for('homefn', name='upload completed'))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port=5001)#host='0.0.0.0',port=5001
